e5c4eea41d983c1c51ba599975ba37d79cae4316
Mehdi Khaleghian <mehdi.khaleghian@readify.net>
2017-10-16 15:33:49 +1100

    show the list of pets on the UI

--- PetDemo/PetDemo.Web/Controllers/HomeController.cs
+++ PetDemo/PetDemo.Web/Controllers/HomeController.cs
@@ -1,11 +1,21 @@
 ﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PetDemo.Model.Enums;
+using PetDemo.Proxy.Interfaces;
 using PetDemo.Web.Models;
 
 namespace PetDemo.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPeopleManager _peopleManager;
+
+        public HomeController(IPeopleManager peopleManager)
+        {
+            _peopleManager = peopleManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -14,6 +24,17 @@ namespace PetDemo.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> People()
+        {
+            var people = await _peopleManager.GetPeopleAsync();
+            var viewModel = new PeopleViewModel();
+            viewModel.MaleOwnedAnimals = people.Where(x => x.Gender == Gender.Male && x.Pets != null)
+                .SelectMany(x => x.Pets).Select(x => x.Name).OrderBy(x => x).ToArray();
+            viewModel.FemaleOwnedAnimals = people.Where(x => x.Gender == Gender.Female && x.Pets != null)
+                .SelectMany(x => x.Pets).Select(x => x.Name).OrderBy(x => x).ToArray();
+            return View(viewModel);
         }
     }
 }
---
+++ PetDemo/PetDemo.Web/Models/PeopleViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PetDemo.Web.Models
+{
+    public class PeopleViewModel
+    {
+        public string[] MaleOwnedAnimals { get; set; }
+        public string[] FemaleOwnedAnimals { get; set; }
+    }
+}
