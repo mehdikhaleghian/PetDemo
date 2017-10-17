8d7b8024b9e2369c314690ee9bcc6955508c6ccc
Mehdi Khaleghian <mehdi.khaleghian@readify.net>
2017-10-17 15:23:11 +1100

    even more tests

--- PetDemo/PetDemo.Tests.Isolation.Web/ModelMappers/CatViewModelMapperTests.cs
+++ PetDemo/PetDemo.Tests.Isolation.Web/ModelMappers/CatViewModelMapperTests.cs
@@ -68,29 +68,61 @@ namespace PetDemo.Tests.Isolation.Web.ModelMappers
         }
 
         [Test]
+        public void Should_Return_The_Correct_Number_Of_Cats_For_MaleOwner_Groups()
+        {
+            var testData = GetTestData();
+            var actual = _sut.Map(testData);
+            actual.MaleOwnedCats.Length.ShouldBeEquivalentTo(
+                testData.Where(x => x.Gender == Gender.Male).Sum(x => x.Pets.Count(pet => pet.Type == PetType.Cat)));
+        }
+
+        [Test]
+        public void Should_Return_The_Correct_Number_Of_Cats_For_FemaleOwner_Groups()
+        {
+            var testData = GetTestData();
+            var actual = _sut.Map(testData);
+            actual.FemaleOwnedCats.Length.ShouldBeEquivalentTo(
+                testData.Where(x => x.Gender == Gender.Female).Sum(x => x.Pets.Count(pet => pet.Type == PetType.Cat)));
+        }
+
+        [Test]
         public void Should_Return_The_Result_Ordered_By_Pets_Names()
         {
-            var actual = _sut.Map(new[] {
+            var testData = GetTestData();
+            var actual = _sut.Map(testData);
+            actual.MaleOwnedCats.First().Should().Be("Max");
+            actual.MaleOwnedCats.Last().Should().Be("Zigi");
+            actual.FemaleOwnedCats.First().Should().Be("Alex");
+            actual.FemaleOwnedCats.Last().Should().Be("Bravy");
+        }
+
+        private Person[] GetTestData()
+        {
+            return new[]
+            {
                 new Person
                 {
-                    Name = "Mehdi", Gender = Gender.Male,
-                    Pets = new[]{new Pet { Name = "Max", Type = PetType.Cat}, new Pet { Name = "James",Type = PetType.Cat} }
+                    Name = "Mehdi",
+                    Gender = Gender.Male,
+                    Pets = new[]
+                    {
+                        new Pet {Name = "Max", Type = PetType.Cat},
+                        new Pet {Name = "Zigi", Type = PetType.Cat},
+                        new Pet {Name = "James", Type = PetType.Dog}
+                    }
 
                 },
                 new Person
                 {
-                    Name = "Mehdi", Gender = Gender.Female,
-                    Pets = new[]{new Pet { Name = "Alex",Type = PetType.Cat}, new Pet { Name = "Bravy",Type = PetType.Cat} }
+                    Name = "Mehdi",
+                    Gender = Gender.Female,
+                    Pets = new[]
+                    {
+                        new Pet {Name = "Alex", Type = PetType.Cat},
+                        new Pet {Name = "Bravy", Type = PetType.Cat}
+                    }
                 }
-            });
-            actual.Should().NotBeNull();
-            actual.NoResult.Should().BeFalse();
-            actual.MaleOwnedCats.Length.Should().Be(2);
-            actual.FemaleOwnedCats.Length.Should().Be(2);
-            actual.MaleOwnedCats.First().Should().Be("James");
-            actual.MaleOwnedCats.Last().Should().Be("Max");
-            actual.FemaleOwnedCats.First().Should().Be("Alex");
-            actual.FemaleOwnedCats.Last().Should().Be("Bravy");
+            };
         }
     }
 }
